e1a69cdb49f758b465b37729fa26345292547104
Vijay Chintalapati <Vijay.Chintalapati@gmail.com>
2018-03-20 12:14:07 -0700

    Auto detection of ALSA card enabled

--- mopidy/run.sh
+++ mopidy/run.sh
@@ -5,6 +5,11 @@ local_scan=$(cat /data/options.json | jq -r '.local_scan // empty')
 options=$(cat /data/options.json | jq -r 'if .options then [.options[] | "-o "+.name+"="+.value ] | join(" ") else "" end')
 config="/var/lib/mopidy/.config/mopidy/mopidy.conf"
 
+# Target ALSA card 
+CARD_INDEX=`cat /proc/asound/cards | egrep '\[ALSA' | awk '{ print $1 }'`
+sed -i 's/defaults\.pcm\.card [0-1]/defaults\.pcm\.card $CARD_INDEX/g' /usr/share/alsa/alsa.conf
+sed -i 's/defaults\.ctl\.card [0-1]/defaults\.ctl\.card $CARD_INDEX/g' /usr/share/alsa/alsa.conf
+
 if  [ "$local_scan" == "true" ]; then
     mopidy --config $config $options local scan
 fi
